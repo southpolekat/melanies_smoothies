c13c767c639c949b971993985bfd40c40629af80
Kat <earthset@gmail.com>
2024-06-07 11:14:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -12,16 +12,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-#option = st.selectbox(
-#    'What is your favourite fruits?',
-#    ('Banana', 'Strawberries','Peaches'))
-#st.write('Your favourite fruit is:', option)
-
-from snowflake.snowpark.functions import col
-
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -36,9 +28,6 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     st.write(ingredients_string)
-
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-        #values ('""" + ingredients_string + """')"""
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
